97170d03cf488853fc0f9daf73385268b37e1c6f
electric-fish <55995360+electric-fish@users.noreply.github.com>
2020-09-23 19:49:35 +0800

    Delete db.config.js

--- database/db.config.js
+++
@@ -1,8 +0,0 @@
-const dbconfig = {
-  username: "admin",
-  password: "adventurerun",
-  db_name: "dotbot-db",
-  uri: `mongodb+srv://admin:adventurerun@cluster0.rb2xk.gcp.mongodb.net/dotbot-db?retryWrites=true&w=majority`,
-};
-
-module.exports = dbconfig;
